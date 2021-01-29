4e879d4008219891e7411b020ac7dd72c67dad46
Mehmet Yazıcı <mehmetyazici.3407@gmail.com>
2021-01-29 23:19:36 +0300

    add ui pages

---
+++ src/ui/bookCreatePageGUI.java
@@ -0,0 +1,17 @@
+package ui;
+
+import javax.swing.*;
+
+public class bookCreatePageGUI {
+    private JPanel mainPanel;
+    private JTextField byXXXXTextField;
+    private JComboBox comboBox1;
+    private JComboBox comboBox3;
+    private JComboBox comboBox2;
+    private JComboBox comboBox4;
+    private JPanel subPanel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
